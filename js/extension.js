02b96a65f8bfb1bb4d0d929fe2de964daf8f8f71
Matt Everson <matt@astuteo.com>
2022-12-03 15:20:58 -0600

    ADD: Working Similar Entries finder

--- js/extension.js
+++ js/extension.js
@@ -25,14 +25,15 @@ function acronym_finder() {
     html += '<h3>Acronym Finder</h3>';
     
     var puzdata = PUZAPP.puzdata;
-    var acronym_clue_lists = [puzdata.across_clues, puzdata.down_clues];
-    for (var j = 0; j < acronym_clue_lists.length; j++) {
-      var acronym_clues = acronym_clue_lists[j];
-      for (var key in acronym_clues) {
-        if (!acronym_clues.hasOwnProperty(key))
+    var clue_lists = [puzdata.across_clues, puzdata.down_clues];
+
+    for (var j = 0; j < clue_lists.length; j++) {
+      var clues = clue_lists[j];
+      for (var key in clues) {
+        if (!clues.hasOwnProperty(key))
             continue;
-        const acronym_result = acronym(acronym_clues[key]);
-        html += '<p><strong>' + acronym_result.toUpperCase() + '</strong> – '+ acronym_clues[key] + '</p>';
+        const acronym_result = acronym(clues[key]);
+        html += '<p><strong>' + acronym_result.toUpperCase() + '</strong> – '+ clues[key] + '</p>';
       }
     }
     document.getElementById('extension0').innerHTML = html;
@@ -42,22 +43,6 @@ function acronym_finder() {
 // -----------------------------
 //  FIND SIMILAR PAIRS OF WORDS
 // -----------------------------
-
-// Helper function to find pairs with a single letter difference
-function singleLetterPairs(words) {
-  const pairs = [];
-  // Loop through the words and compare each word to every other word
-  for (let i = 0; i < words.length; i++) {
-    for (let j = 0; j < words.length; j++) {
-      // Check if the words are not the same word and have only one letter different
-      if (i !== j && diff(words[i], words[j]) === 1) {
-        // Add the pair to the array
-        pairs.push([words[i], words[j]]);
-      }
-    }
-  }
-  return pairs;
-}
 
 // Helper function to count the number of differences between two words
 function diff(a, b) {
@@ -70,23 +55,51 @@ function diff(a, b) {
   return diff;
 }
 
+// Helper function to find pairs with a single letter difference
+function singleLetterPairs(words) {
+  const pairs = [];
+  // Loop through the words and compare each word to every other word
+  for (let i = 0; i < words.length; i++) {
+    for (let j = 0; j < words.length; j++) {
+      // Check if the words are not the same word, have only one letter different, are the same length, and are in alphabetical order
+      if (i !== j && diff(words[i], words[j]) === 1 && words[i].length === words[j].length && words[i] < words[j]) {
+        // Add the pair to the array
+        pairs.push([words[i], words[j]]);
+      }
+    }
+  }
+  return pairs;
+}
+
 function very_similar_words() {  
 
   var html = '';
   html += '<br><h3>Very Similar Words</h3>';
 
-  const words = ["tour", "force", "nom", "plume", "sour", "forte", "non", "plumb"];
-  const pairs = singleLetterPairs(words);
-  var pair_matches = [];
-  for (const [word1, word2] of pairs) {
-    pair_matches.push(`${word1.toUpperCase()} ${word2.toUpperCase()}`);
-  }
+  var puzdata = PUZAPP.puzdata;
+  var entry_lists = [puzdata.across_entries, puzdata.down_entries];
 
-  if (pair_matches.length === 0) {
-    html += '<p>No very similar words found</p>';
-  } else {
-    for (const pair of pair_matches) {
-      html += '<p>' + pair + '</p>';
+  for (var j = 0; j < entry_lists.length; j++) {
+
+    // Convert the entries key/value object into an array of only words
+    var entries = entry_lists[j];
+    var values = Object.values(entries);
+    var words = values.map(value => value);
+
+    // Find similar pairs from the words array
+    const pairs = singleLetterPairs(words);
+    console.log(pairs);
+    var pair_matches = [];
+    for (const [word1, word2] of pairs) {
+      pair_matches.push(`${word1.toUpperCase()} ${word2.toUpperCase()}`);
+    }
+  
+    if (pair_matches.length === 0) {
+      html += '<p>No very similar words found</p>';
+    } else {
+      for (const pair of pair_matches) {
+        html += '<p>' + pair + '</p>';
+      }
     }
   }
   document.getElementById('extension1').innerHTML = html;
